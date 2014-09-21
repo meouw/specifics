9ea65d733270c75171d61d700164daa2e6d2f4ac
Clive Calmeyer <clive'meouw.com>
2014-09-21 22:44:07 +0100

    Improved comments on test

--- tests/Meouw/Type/EnumTest.php
+++ tests/Meouw/Type/EnumTest.php
@@ -13,6 +13,9 @@ class EnumTest extends \PHPUnit_Framework_TestCase
 		'SPADES' => 'spades'
 	);
 
+    /**
+     * Tests that an Enum can be constructed
+     */
 	public function testConstruct()
 	{
 		$enum = new Suit(Suit::CLUBS);
@@ -20,25 +23,38 @@ class EnumTest extends \PHPUnit_Framework_TestCase
 		$this->assertInstanceOf('Meouw\Type\Subject\Suit', $enum);
 	}
 
-	public function testConstruct_InvalidValue_ThrowsException()
+    /**
+     * Tests that an Enum can only be constructed with one of its allowed values
+     *
+     * @expectedException \UnexpectedValueException
+     */
+	public function testConstructWithInvalidValueThrowsException()
 	{
-		$this->setExpectedException('UnexpectedValueException');
 		new Suit('bogus');
 	}
 
+    /**
+     * Tests that the value of an Enum can be retrieved
+     */
 	public function testGetValue()
 	{
 		$enum = new Suit(Suit::CLUBS);
 		$this->assertEquals('clubs', $enum->getValue());
 	}
 
+    /**
+     * Tests that the name of the constant on the Enum class can be retrieved
+     */
 	public function testGetKey()
 	{
 		$enum = new Suit(Suit::CLUBS);
 		$this->assertEquals('CLUBS', $enum->getKey());
 	}
 
-	public function testGetList_AsStrings()
+    /**
+     * Tests that an array of all allowable values can be retrieved as strings
+     */
+	public function testGetListAsStrings()
 	{
 		$list = Suit::getList();
 		$this->assertEquals(
@@ -47,7 +63,10 @@ class EnumTest extends \PHPUnit_Framework_TestCase
 		);
 	}
 
-	public function testGetList_AsObjects()
+    /**
+     * Tests that an array of all allowable values can be retrieved as Enum objects
+     */
+	public function testGetListAsObjects()
 	{
 		$list = Suit::getList(true);
 
@@ -58,6 +77,9 @@ class EnumTest extends \PHPUnit_Framework_TestCase
 		}
 	}
 
+    /**
+     * Tests that it is possible to check whether a value is allowable for an enum
+     */
 	public function testHas()
 	{
 		foreach ($this->list as $value) {
